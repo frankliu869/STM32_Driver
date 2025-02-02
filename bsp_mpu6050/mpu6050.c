29aac9b4cc2ef7efb1aa2604bf6f4673d5b69d95
Frank Liu <frankliu869@gmail.com>
2025-02-02 22:26:40 +0800

    mpu6050

---
+++ bsp_mpu6050/mpu6050.c
@@ -0,0 +1,24 @@
+/**
+  ******************************************************************************
+  * @file    mpu6050.h
+  * @author  NTUT Fryan Liu
+  * @brief   Header file of MPU6050 driver  
+  ******************************************************************************
+  */
+
+#include "mpu6050.h"
+
+MPU6050_StatusTypeDef MPU6050_Init(void)
+{
+  return MPU6050_OK;
+}
+
+MPU6050_StatusTypeDef MPU6050_ReadAccel(uint16_t *pBuffer)
+{
+  return MPU6050_OK;
+}
+
+MPU6050_StatusTypeDef MPU6050_ReadGyro(uint16_t *pBuffer)
+{
+  return MPU6050_OK;
+}
